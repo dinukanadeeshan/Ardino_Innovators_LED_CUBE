06f990f093643f90de2d921b19bd96ca3231c9a5
dinukanadeeshan <dinuka.nadeeshan@yahoo.com>
2016-07-28 06:09:10 +0530

    Added countdown pattern

---
+++ LED Patterns/CountDown/CountDown.ino
@@ -0,0 +1,24 @@
+#include<stdlib.h>
+int sw = 1;
+int n1 = 2;
+int n2 = 3;
+int n3 = 4;
+int p1 = 5;
+int p2 = 6;
+int p3 = 7;
+int p4 = 8;
+int p5 = 9;
+int p6 = 10;
+int p7 = 11;
+int p8 = 12;
+int p9 = 13;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
